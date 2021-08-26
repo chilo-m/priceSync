2fd11bc23d5ab961efc898263b48ba21db363178
marrik98 <mark141198@yandex.ru>
2021-08-26 21:51:04 +0300

    fixed errors in reading xml of products without prices

--- priceSync.py
+++ priceSync.py
@@ -1,8 +1,6 @@
 import csv
 from typing import Counter
 import xml.etree.ElementTree as ET
-#import msvcrt
-
 
 class product:
     # класс товара
@@ -71,34 +69,49 @@ class product:
         # вывод информации о товаре
         print("Код артикула: {} Цена: {} Остаток: {}".format(self.art, self.price, self.count))
 
+    # для артикулов начинающихся с 999 которых нет в базе 1с
+    def art_is_new(self, s=""): 
+        if not s: 
+            s = str(self.get_art())
+        return (len(s) > 5 and s[:3] == "999") 
+
 class productList(product):
     # класс списка товаров
     
-    arrCurProducts = []
-    arrNewProducts = []   
+    arrSiteProducts = [] # товары с сайта
+    arr1СProducts = [] # товары с 1С
+    arrNewProducts = [] # товары на сайте, которых нет в 1с\
     
-    def append_cur(self,Product):
-        self.arrCurProducts.append(Product)
+    def append_site(self,Product):
+        self.arrSiteProducts.append(Product)
+
+    def append_1c(self,Product):
+        self.arr1СProducts.append(Product)
 
     def append_new(self,Product):
         self.arrNewProducts.append(Product)
 
-    def info_cur(self):
+    def info_site(self):
         print("Товары на сайте:")
-        for p in self.arrCurProducts:   
+        for p in self.arrSiteProducts:   
+            print(p)
+
+    def info_1c(self):
+        print("Товары в 1С:")
+        for p in self.arr1СProducts:   
             print(p)
 
     def info_new(self):
-        print("Товары в 1С:")
+        print("Новые товары:")
         for p in self.arrNewProducts:   
             print(p)
 
     def __str__(self):
         s = "Товары на сайте:\n"
-        for p in self.arrCurProducts:
+        for p in self.arrSiteProducts:
             s+= "{}\n".format(p.__str__())
         s+= "Товары в 1С:\n"
-        for p in self.arrNewProducts:
+        for p in self.arr1СProducts:
             s+= "{}\n".format(p.__str__())
         return s
 
@@ -108,7 +121,10 @@ class productList(product):
         reader = csv.DictReader(fcur, delimiter=';')
         for row in reader:
             if (row["Тип строки"] == "product_variant" or row["Тип строки"] == "variant"):
-                self.append_cur(product(Art=row["Код артикула"], Price=row["Цена"]))
+                if self.art_is_new(row["Код артикула"]):
+                    self.append_new(product(Art=row["Код артикула"], Price=row["Цена"]))
+                else:    
+                    self.append_site(product(Art=row["Код артикула"], Price=row["Цена"]))
         print("---Чтение CSV завершено---")
     
     def xml_dict_reader_1c(self, fimport, foffers):
@@ -124,7 +140,7 @@ class productList(product):
                 if valueProp.find('Наименование').text == "Код":
                     # Получение значения Кода артикула                
                     art = valueProp.find('Значение').text
-            self.append_new(product(ID = id, Art = art))
+            self.append_1c(product(ID = id, Art = art))
         
         print("{} done".format(fimport.name))
 
@@ -137,44 +153,46 @@ class productList(product):
         for prod in root.iter('Предложение'):
             
             id = prod.find('Ид').text
-            if id == self.arrNewProducts[i].get_id():
+            if id == self.arr1СProducts[i].get_id():
                 try:
-                    self.arrNewProducts[i].set_price(prod.find("Цены/Цена/ЦенаЗаЕдиницу").text)
-                    self.arrNewProducts[i].set_count(prod.find('Количество').text)
+                    self.arr1СProducts[i].set_price(prod.find("Цены/Цена/ЦенаЗаЕдиницу").text)
+                    self.arr1СProducts[i].set_count(prod.find('Количество').text)
                     i+=1
                 except AttributeError:
-                    print("Ошибка в чтении файла!\nТовар не распознан 'id': '{}'".format(id))
+                    self.arr1СProducts.pop(i)
+                    # print("Ошибка в чтении файла! У товара не удалось найти атрибут 'Цена' или 'Количество'\nТовар не распознан 'id': '{}'".format(id))
+                continue
             else:
-                """
-                попытка найти соответствие товаров при несовпадающих номерах
-                Будет долго из-за большой номенклатуры
-                Проверяется по списку файла offers.xml в котором есть цены и остатки товаров,
-                поэтому если элемента не будет в import.xml,
-                то у товара останутся поля Стоимость и Количество по умолчанию 0.
-                Если будет найден товар в offers.xml, а артикул его не будет найден, то выдет сообщение об ошибке
-                """
-                try:
-                    check_j = False
-                    for j in self.arrNewProducts:
-                        if id == j.get_id():
-                            i = self.arrNewProducts.index(j)                            
-                            self.arrNewProducts[i].set_price(prod.find("Цены/Цена/ЦенаЗаЕдиницу").text)
-                            self.arrNewProducts[i].set_count(prod.find('Количество').text)
+                
+                # попытка найти соответствие товаров при несовпадающих номерах
+                # Будет долго из-за большой номенклатуры
+                # Проверяется по списку файла offers.xml в котором есть цены и остатки товаров,
+                # поэтому если элемента не будет в import.xml,
+                # то у товара останутся поля Стоимость и Количество по умолчанию 0.
+                # Если будет найден товар в offers.xml, а артикул его не будет найден, то выдет сообщение об ошибке
+                
+                check_j = False
+                for j in self.arr1СProducts:
+                    if id == j.get_id():
+                        try:
+                            i = self.arr1СProducts.index(j)                            
+                            self.arr1СProducts[i].set_price(prod.find("Цены/Цена/ЦенаЗаЕдиницу").text)
+                            self.arr1СProducts[i].set_count(prod.find('Количество').text)
                             i+=1
-                            check_j = True
-                            break
-                    if check_j == False:
-                        print("Артикул не найден! Данные товара:\n\"{}\"\nid = {} не найдены.\nПроверьте правильность выгрузки БД".format(prod.find('Наименование').text, id))
-                except:
-                    print("Ошибка в файлах выгрузки") 
-                    #return
+                        except AttributeError:
+                            self.arr1СProducts.pop(i)
+                            # print("Ошибка в чтении файла! У товара не удалось найти атрибут 'Цена' или 'Количество'\nТовар не распознан 'id': '{}'".format(id))
+                        check_j = True
+                        break
+                if check_j == False:
+                    print("Артикул не найден! Данные товара:\n\"{}\"\nid = {} не найдены.\nПроверьте правильность выгрузки БД".format(prod.find('Наименование').text, id))
 
         print("{} done".format(foffers.name))
 
         print("Проверка целостности...")     
-        #for prod in self.arrNewProducts:
-            #if prod.get_price() == 0:
-                #print("Ошибка при проверке целостности в файлах выгрузки!\n Товар с id: {} артикулом: {} без цены!".format(prod.get_id(), prod.get_art()))
+        for prod in self.arr1СProducts:
+            if prod.get_price() == 0 and not (prod.get_count() == 0):
+                print("Ошибка при проверке целостности в файлах выгрузки!\n Товар с id: {} артикулом: {} без цены!".format(prod.get_id(), prod.get_art()))
         print("Проверка целостности завершена!") 
         print("---Чтение XML завершено---")
         return
@@ -187,17 +205,16 @@ class productList(product):
         print("---Поиск изменений---")
 
         # Поиск циклом соответствий артикулов
-        for curProd in self.arrCurProducts:
+        for curProd in self.arrSiteProducts:
             artTest = False
-            for newProd in self.arrNewProducts:                
+            for newProd in self.arr1СProducts:                
                 if curProd.get_art() == newProd.get_art():
                     # Артикул найден
                     artTest = True
-                    # Проверка наличия изменений
-                    """
-                    if (curProd.get_price() != newProd.get_price()
-                    or  curProd.get_count() != newProd.get_count()):
-                    """
+                    # Проверка наличия изменений цены и количества
+                    #if (curProd.get_price() != newProd.get_price()
+                    #or  curProd.get_count() != newProd.get_count()):
+                    
                     # Проверка, выросли ла цена
                     if (curProd.get_price() < newProd.get_price()):
 
@@ -205,17 +222,12 @@ class productList(product):
                         curProd.set_count(newProd.get_count())
                         inner_dict = dict(zip(self.fieldnames, [curProd.get_art(),curProd.get_price(),curProd.get_count()]))
                         dataList.append(inner_dict)
-                        #print("There ara changes: {}".format(inner_dict))
-
-                        #print("cur Артикул: {} Цена: {}\nnew Артикул: {} Цена: {} Id: {}".format(curProd.get_art(),curProd.get_price(),newProd.get_art(),newProd.get_price(), newProd.get_id()))
-                    #else:
-                        #print("There ara NO changes 'Код артикула': {}".format(newProd.get_art()))
                     break
             try:
                 if artTest == False:
                     print("Критическая ошибка! Артикул с сайта не найден в файлах 1с!\nАртикул: {} Id: {}".format(curProd.get_art()))
             except IndexError:
-                print("Ошибка переполнения массива")
+                print("Ошибка переполнения массива") 
         print("---Поиск изменений завершен!---")
         return dataList
 
@@ -232,9 +244,7 @@ class productList(product):
         
 if __name__ == "__main__":
 
-    tp = product(1,2,3,4)
-
-    pl = productList()    
+    pl = productList()
     
     with open("products.csv") as fcur_obj:
         pl.csv_dict_reader_site(fcur_obj)
@@ -245,6 +255,6 @@ if __name__ == "__main__":
     with open("update.csv", 'w') as fupdate_obj:
         pl.csv_dict_writer_site(fupdate_obj)
 
-    #pl.info_cur()
+    pl.info_new()
 
     print ('конец')
